48dddebc2e4e2a1ab5c22ff2acd20b20150cf76c
Thomas Joyeux <tjoyeux@paul-f4Br5s8.clusters.42paris.fr>
2023-12-24 18:21:38 +0100

    To Do : Gestion of arguments

---
+++ push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjoyeux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/24 16:15:35 by tjoyeux           #+#    #+#             */
+/*   Updated: 2023/12/24 18:09:13 by tjoyeux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+
+//char	**ft_split(char const *s, char c);
+#endif
